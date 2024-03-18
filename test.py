bfcce00b22346cbd9b53fe775bea56ea775f0fe9
ry461ch <ryabov.kolya20013@gmail.com>
2024-03-19 00:28:40 +0400

    test_2

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import os
-
 a = 4
 b = 6
-print(a + b)
-s = {
-    'a': 'b',
-    't': 'y',
-    'h': 'h'
-}
-c = a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + \
-    a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a
-print(c + b)
+c = {'a': 'b', 'c': 'd'}
+print(a+b)
+print(c)
